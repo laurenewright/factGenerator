7cbbb53c143f673132903e25203589ac62eb9e37
lauren wright <wright.lauren20@gmail.com>
2018-10-07 18:55:30 -0400

    facts now display in the console when button is clicked

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
 // going to add an api request to a random fact generator
 // this one being a possibility https://fact.birb.pw/
+
+const endpoint = 'https://api.chucknorris.io/jokes/random';
+
+function getFact() {
+    fetch(endpoint)
+        .then(response => response.json())
+        .then(data => console.log(data.value))
+}
+
+function displayFact(fact){
+    const factText = document.querySelector('.fact-text');
+    factText.textContent = fact;
+}
+
+const newFact = document.querySelector('.fact');
+newFact.addEventListener('click', getFact);
